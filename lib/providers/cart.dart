1e3d28e63d7e38beb919de0ce7c6edbd375a47a0
boyk0 <v_boiko-itssv@kw.com>
2022-08-01 16:25:49 +0300

    add cart provider

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -15,10 +15,14 @@ class CartItem {
 }
 
 class Cart with ChangeNotifier {
-  Map<String, CartItem> _items;
+  Map<String, CartItem> _items = {};
 
   Map<String, CartItem> get items {
     return {..._items};
+  }
+
+  int get itemCount {
+    return _items.length;
   }
 
   void addItem(String productId, double price, String title) {
@@ -36,5 +40,6 @@ class Cart with ChangeNotifier {
         quantity: 1,
         price: price));
     }
+    notifyListeners();
   }
 }

6fc56d2b65cf2894c326dfdc14a5eb931c76d5bf
boyk0 <v_boiko-itssv@kw.com>
2022-08-04 23:26:15 +0300

    adding & editing product

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/products.dart';
+import '../widgets/app_drawer.dart';
 import '../widgets/user_product_item.dart';
 import 'edit_product_screen.dart';
 
@@ -24,6 +25,7 @@ class UserProductsScreen extends StatelessWidget{
           )
         ],
       ),
+      drawer: AppDrawer(),
       body: Padding(
         padding: EdgeInsets.all(8),
         child: ListView.builder(

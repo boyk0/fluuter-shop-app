d9868bedc61c6312cd6083358e0a1084cebb7d1d
boyk0 <v_boiko-itssv@kw.com>
2022-08-08 15:56:32 +0300

    add loading indicator

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -9,6 +9,10 @@ import 'edit_product_screen.dart';
 class UserProductsScreen extends StatelessWidget{
   static const String routeName = '/user-products';
 
+  Future<void> _refreshProducts(BuildContext context) async {
+    await Provider.of<Products>(context, listen: false).fetchAndSetProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
@@ -26,16 +30,19 @@ class UserProductsScreen extends StatelessWidget{
         ],
       ),
       drawer: AppDrawer(),
-      body: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListView.builder(
-          itemBuilder: (ctx, i) => Column(
-            children: [
-              UserProductItem(productsData.items[i]),
-              Divider(),
-            ],
+      body: RefreshIndicator(
+        onRefresh: () => _refreshProducts(context),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListView.builder(
+            itemBuilder: (ctx, i) => Column(
+              children: [
+                UserProductItem(productsData.items[i]),
+                Divider(),
+              ],
+            ),
+            itemCount: productsData.items.length,
           ),
-          itemCount: productsData.items.length,
         ),
       ),
     );
